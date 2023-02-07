30b6cb86232eddb96fd9f750ad2a0b40c99c2137
Blease Carson <bleasecarson@gmail.com>
2023-02-07 13:24:29 -0500

    Initial commit

---
+++ sample-project/env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib"
+XML2_LIBS="-lxml2 -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -lz -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -llzma -lpthread -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -liconv -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/saebel/Desktop/machine-learning-projects/sample-project/env/include/libxml2 -I/Users/saebel/Desktop/machine-learning-projects/sample-project/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ sample-project/env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib"
+XSLT_LIBS="-lxslt -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -lxml2 -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -lpthread -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -liconv -L/Users/saebel/Desktop/machine-learning-projects/sample-project/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/saebel/Desktop/machine-learning-projects/sample-project/env/include"
+MODULE_VERSION="xslt-1.1.35"
