f6856a270c4dd69aa0f245483a80012674c77ebf
Blease Carson <bleasecarson@gmail.com>
2023-02-11 13:02:50 -0500

    Removed bulldozer project

--- bulldozer-sales-project/env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/saebel/machine-learning-projects/bulldozer-sales-project/env/lib"
-XML2_LIBS="-lxml2 -L/Users/saebel/machine-learning-projects/bulldozer-sales-project/env/lib -lz -L/Users/saebel/machine-learning-projects/bulldozer-sales-project/env/lib -llzma -lpthread -L/Users/saebel/machine-learning-projects/bulldozer-sales-project/env/lib -liconv -L/Users/saebel/machine-learning-projects/bulldozer-sales-project/env/lib -licui18n -licuuc -licudata -lm "
-XML2_INCLUDEDIR="-I/Users/saebel/machine-learning-projects/bulldozer-sales-project/env/include/libxml2 -I/Users/saebel/machine-learning-projects/bulldozer-sales-project/env/include"
-MODULE_VERSION="xml2-2.9.14"
-
